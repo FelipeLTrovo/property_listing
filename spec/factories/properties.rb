4ba76b5f615068065ad1f7d16b170585c684db1f
FelipeLTrovo <felipeltrovo@hotmail.com>
2022-12-18 21:14:55 -0300

    Creates a factory for Property.

--- spec/factories/properties.rb
+++ spec/factories/properties.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :property do
-    name { "MyString" }
+    name {"My Property"}
+    photos {[ Rack::Test::UploadedFile.new(Rails.root.join("spec/support/photos/property_photo.jpeg")),
+              Rack::Test::UploadedFile.new(Rails.root.join("spec/support/photos/property_photo.jpeg")),
+              Rack::Test::UploadedFile.new(Rails.root.join("spec/support/photos/property_photo.jpeg"))
+            ]}
   end
 end
