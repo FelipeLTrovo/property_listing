a7735a1ff637dd9f9df782f2358f7486de10db9b
FelipeLTrovo <felipeltrovo@hotmail.com>
2022-12-18 21:53:17 -0300

    Adds cover_photo method to Property model.

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,14 @@ class Property < ApplicationRecord
   has_many_attached :photos
 
   validates :name, presence: true
+
+  def cover_photo
+    return if photos.nil?
+
+    if photos.size < 3
+      return photos.last
+    else
+      return photos[2]
+    end
+  end
 end
