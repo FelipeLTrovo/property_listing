1bd815c2071af55a3e8afcaf0d9ca7e4f8d69ff0
FelipeLTrovo <felipeltrovo@hotmail.com>
2022-12-19 01:50:49 -0300

    Adds seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+50.times do
+  x = Property.create!(name: Faker::Address.full_address)
+  2..6.times do
+    x.photos.attach(io: File.open((Dir.glob('app/assets/images/real_state/*.jpg').sample)), filename: 'photo.jpg')
+    x.save!
+  end
+end
